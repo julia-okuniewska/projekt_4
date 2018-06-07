aecbfe7f35ddbe65c9a10de3e72ad99a7fc9986f
perchfish9 <37780140+perchfish9@users.noreply.github.com>
2018-06-07 18:32:19 +0200

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+
+#include "myview.h"
+#include "ramie.h"
 
 namespace Ui {
 class MainWindow;
@@ -13,13 +17,27 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void narysuj_ramie(QPoint p0, QPoint p1);
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void rysuj( QPoint p0, QPoint p1, QPoint p2 );
 
 private:
     Ui::MainWindow *ui;
+    myGraphicsView * scena;
+
+    QGraphicsScene *s;
+
+    QRectF rect;
+
+    QVector<QGraphicsItem*> klocki;
+    QGraphicsItem * trzymany;
+
+    QPixmap bck;
+    QPixmap nju;
+
+    ramie * robot;
 };
 
 #endif // MAINWINDOW_H
